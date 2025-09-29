3413493257c283dafc3d751eabb5554d36f33399
cauwel <patrick.cauwel@darva.com>
2025-09-29 11:25:19 +0200

    fleché

--- 04_arrow.js
+++ 04_arrow.js
@@ -7,7 +7,7 @@
  *   - le mot clée "function" est interdis
  */
 
-let arrow1 = () => {}
+let arrow1 = (a) => {return a+1}
 
 /**
  * Déclarer une fonction fléchée qui renvoi l'argument + 1
@@ -18,6 +18,10 @@ let arrow1 = () => {}
  *   - le mot clée "return" est interdis
  */
 
-let arrow2 = () => {}
+let arrow2 = (a) => a+1
+
+
+console.log(arrow1(66))
+console.log(arrow1(67))
 
 module.exports = {arrow1, arrow2};
